2f2f67c034730d3cdce0ca05312a803c8dd40a81
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2025-02-04 11:47:07 -0300

    feat: create methods

---
+++ src/models/BookList.js
@@ -0,0 +1,21 @@
+class BookList {
+    constructor() {
+        this.books = [];
+    }
+
+    getAllBooks(){
+        return this.books;
+    }
+
+    getBookById(id){
+        
+    }
+
+    addBook(book){
+        this.books.push(book);
+    }
+
+    updateBook(id, book){
+        
+    }
+}
