e2145f31c1f4b63326e1c188ebdf3b073d57d926
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2025-02-06 13:18:25 -0300

    feat: finished create methods: models

--- src/models/BookList.js
+++ src/models/BookList.js
@@ -3,19 +3,33 @@ class BookList {
         this.books = [];
     }
 
-    getAllBooks(){
+    getAllBooks() {
         return this.books;
     }
 
-    getBookById(id){
-        
+    getBookById(id) {
+        const book = this.books.find(book => book.id == id);
+        if(!song) {
+            throw new Error('Book not found');
+        }
+        return book;
     }
 
-    addBook(book){
+    addBook(book) {
         this.books.push(book);
     }
 
-    updateBook(id, book){
-        
+    updateBook(id, updateData) {
+        const book = this.getBookById(id);
+        Object.assign(book, updateData);
+        return book;
+    }
+
+    deleteBook(id) {
+        this.books = this.books.filter(book => book.id != id);
+    }
+
+    getAllTotalBooks() {
+        return this.books.length;
     }
 }
